e069b94f917e26118e29962cebe8cdcd2db306bc
Junaid Arshad <jun.arshad@gmail.com>
2022-12-21 18:33:15 -0500

    Created Message entity which contains author, array of command and it's target, and the message to be delivered

---
+++ src/main/java/entities/Message.java
@@ -0,0 +1,63 @@
+package main.java.entities;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class Message {
+    private final String author;
+    private final String[] command = new String[2];
+    private String msg;
+    //TODO: Move placeholder commands so that they're not tied to entity
+    static final List<String> TARGETTED_COMMANDS = Arrays.asList("dm", "setname");
+    static final List<String> COMMANDS = Arrays.asList("help", "quit", "q");
+
+    Message(String author, String str) {
+        this.author = author;
+
+        if (str.startsWith("/")) {
+            setCommandTargetMsg(str);
+        }
+        else {
+            this.command[0] = "broadcast";
+            this.msg = str;
+        }
+    }
+
+    void setCommandTargetMsg(String str) {
+        String[] CommandMsg = str.split(" ", 2);
+
+        this.command[0] = CommandMsg[0].substring(1);
+
+        if (TARGETTED_COMMANDS.contains(this.command[0])) {
+            setTargetMessage(CommandMsg[1]);
+        }
+    }
+
+    void setTargetMessage(String str) {
+        String[] TargetMsg = str.split(" ", 2);
+        this.command[1] = TargetMsg[0];
+        if (TargetMsg.length == 2) {
+            this.msg = TargetMsg[1];
+        }
+    }
+
+    @Override
+    public String toString() {
+        return "Message{" +
+                "author='" + author + '\'' +
+                ", command=" + Arrays.toString(command) +
+                ", msg='" + msg + '\'' +
+                '}';
+    }
+
+    public static void main(String[] args) {
+        Message m1 = new Message("Dr.Seuss", "Hello everyone!");
+        Message m2 = new Message("Dr.Seuss", "/help");
+        Message m3 = new Message("Dr.Seuss", "/dm GeorgeOrwell Hello everyone!");
+        Message m4 = new Message("Dr.Seuss", "/setname GeorgeOrwell");
+        System.out.println(m1);
+        System.out.println(m2);
+        System.out.println(m3);
+        System.out.println(m4);
+    }
+}
