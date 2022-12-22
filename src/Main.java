1f7e47187a91ac3d297b3ceb906f481a30368139
Junaid Arshad <jun.arshad@gmail.com>
2022-12-22 07:22:58 -0500

    Wrote some quick tests in main.java, will write proper tests shortly

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,21 @@
+import main.java.entities.Command;
+import main.java.entities.Message;
+import main.java.features.writeMessage.WriteMessageInputBoundary;
+import main.java.features.writeMessage.WriteMessageInteractor;
+import main.java.features.writeMessage.WriteMessageResponseModel;
+import main.java.interfaceAdapters.WriteMessageController;
+import main.java.interfaceAdapters.WriteMessagePresenter;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        WriteMessagePresenter presenter = new WriteMessagePresenter();
+        WriteMessageInputBoundary writeMessageData = new WriteMessageInteractor(null, presenter);
+        WriteMessageController controller = new WriteMessageController(writeMessageData);
+        Message msg01 = new Message.Builder().setAuthor("Jun").setCommandAndMessage("/dm John test").build();
+        System.out.println(msg01);
+        Command c = new Command("quit", null);
+        Message msg02 = new Message("Jun", c, "test");
+        System.out.println(msg02);
+        WriteMessageResponseModel success = controller.create("Jimmetheus", "/dm john Hey is everyone getting this?");
     }
 }
