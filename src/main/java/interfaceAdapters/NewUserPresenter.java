f6fec8603876f83272c8d4673e3bcc184bebea7b
Junaid Arshad <jun.arshad@gmail.com>
2022-12-27 20:36:14 -0500

    Implemented Presenter for NewUser usecase

--- src/main/java/interfaceAdapters/NewUserPresenter.java
+++ src/main/java/interfaceAdapters/NewUserPresenter.java
@@ -1,4 +1,18 @@
 package main.java.interfaceAdapters;
 
-public class NewUserPresenter {
+import main.java.features.NewUser.NewUserFailedException;
+import main.java.features.NewUser.NewUserOutputBoundary;
+import main.java.features.NewUser.NewUserResponseModel;
+
+public class NewUserPresenter implements NewUserOutputBoundary {
+
+    @Override
+    public NewUserResponseModel presentSuccess(NewUserResponseModel responseModel) {
+        return responseModel;
+    }
+
+    @Override
+    public NewUserResponseModel presentError(String error) {
+        throw new NewUserFailedException(error);
+    }
 }
